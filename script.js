d325aa08e2b87fccd9e385ffcccceeffc0e6fa1c
Joey Westhoff <jmw8033@psu.edu>
2023-07-11 15:04:09 -0400

    Sum
    test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// JavaScript code goes here
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", function() {
+    // Find all anchor tags with the 'data-link-text' attribute
+    var links = document.querySelectorAll("a[data-link-text]");
+  
+    // Loop through each link and update its text
+    links.forEach(function(link) {
+      var linkText = link.getAttribute("data-link-text");
+      link.textContent = linkText;
+    });
+  });
